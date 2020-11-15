7bf451eaf228236ef0d8bf8a16ef5244cfad0761
Rohan Kandi <kandirohan1@gmail.com>
2020-11-15 01:55:25 -0500

    New Functions added - Added login function - Added register function - Changed Login method to a GET from POST - Minor changes to requirements file and others.

---
+++ uFinance Back End/Test.sql
@@ -0,0 +1,3 @@
+-- INSERT INTO Users
+-- VALUES ("david@yahoo.com", "abcdefg", "david", "David", "Silva", "2003-11-02 00:00:00");
+SELECT * FROM Users;
--- uFinance Back End/uFinance SAM Deployment/handlers/register.py
+++ uFinance Back End/uFinance SAM Deployment/handlers/register.py
@@ -1,11 +1,161 @@
 import json
+import re
+import pymysql.cursors
+import datetime
 
-# import requests
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+##### Needed #####
+# Check for similar username or email.
+# Encrypt the password with SHA256.
 
 def lambda_handler(event, context):
+    # Parameter variables
+    EMAIL = event.get("email")
+    PASSWORD = event.get("password")
+    USERNAME = event.get('username')
+    FIRST_NAME = event.get('first_name')
+    LAST_NAME = event.get('last_name')
+    register_status = False
+
+    # ENV Variables
+    ENDPOINT = os.environ.get('db_endpoint')
+    USR = os.environ.get('usr')
+    PORT = int(os.environ.get('port'))
+    REGION = os.environ.get('region')
+    DB_PASS = os.environ.get('db_pass')
+    DBNAME = os.environ.get('db_name')
+
+    # Useful Variables
+    error_message = ""
+    email_regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    pass_regex = '[\^\'\"?\.\ ]'
+
+    # Basic input sanitization
+    # Email
+    if re.search(email_regex, EMAIL):
+        register_status = True
+    else:
+        error_message = "Improper email input"
+        register_status = False
+
+    if not register_status:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "register_status": register_status,
+                "message": error_message,
+            })
+        }
+
+    # Password
+    if re.search(pass_regex, PASSWORD):
+        error_message = "Imporper Password:Invalid character input"
+        register_status = False
+    else:
+        register_status = True
+
+    if not register_status:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "register_status": register_status,
+                "message": error_message,
+            })
+        }
+
+    # Username
+    if re.search(pass_regex, PASSWORD):
+        error_message = "Imporper ID:Invalid character input"
+        register_status = False
+    else:
+        register_status = True
+
+    if not register_status:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "register_status": register_status,
+                "message": error_message,
+            })
+        }
+
+    # First Name
+    if re.search(pass_regex, FIRST_NAME):
+        error_message = "Imporper FName:Invalid character input"
+        register_status = False
+    else:
+        register_status = True
+
+    if not register_status:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "register_status": register_status,
+                "message": error_message,
+            })
+        }
+
+    # Last Name
+    if re.search(pass_regex, FIRST_NAME):
+        error_message = "Imporper LName:Invalid character input"
+        register_status = False
+    else:
+        register_status = True
+
+    if not register_status:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "register_status": register_status,
+                "message": error_message,
+            })
+        }
+
+    #Datetime
+    dt = datetime.datetime.now()
+
+    # Add account to the database.
+    query_results = None
+    try:
+        conn =  pymysql.connect(host=ENDPOINT, user=USR, passwd=DB_PASS, port=PORT, database=DBNAME)
+        cur = conn.cursor()
+        query = """
+                INSERT INTO Users
+                VALUES (\"{}\", \"{}\", \"{}\", \"{}\", \"{}\", \"{}\");
+                """.format(EMAIL, PASSWORD, USERNAME, FIRST_NAME, LAST_NAME, dt)
+        cur.execute(query)
+        conn.commit()
+    except Exception as e:
+        print("Database connection failed due to {}".format(e))
+        error_message = "Database connection error"
+        register_status = False
+    finally:
+        conn.close()
+
+    if register_status:
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "register_status": register_status,
+                "message": "Success"
+            }),
+        }
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "register_status": "true",
-        }),
-    }
+            "statusCode": 400,
+            "body": json.dumps({
+                "register_status": register_status,
+                "message": error_message
+            }),
+        }
+
+# print(lambda_handler({
+#   "email": "daniel@yahoo.com",
+#   "password": "password!",
+#   "username": "dtsu",
+#   "first_name": "Daniel",
+#   "last_name": "Tsu"
+# }, None)
+# )
