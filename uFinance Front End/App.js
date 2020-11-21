dc259f303c7acbbff1f2b00bd9ed30c575109783
danielsst1 <daniel.tse@nyu.edu>
2020-11-20 19:10:48 -0500

    cleaned code

--- uFinance Front End/App.js
+++ uFinance Front End/App.js
@@ -40,14 +40,6 @@ const Tab = createBottomTabNavigator();
 
 const HomeStack = createStackNavigator();
 
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3({accessKeyId:TEST_KEY, secretAccessKey:TEST_SECRET_KEY, region:TEST_REGION});
-
-var params = {Bucket: 'test-bucket-tutorial', Key: 'images/myimage.jpg', ContentType: 'image/jpeg'};
-s3.getSignedUrl('putObject', params, function (err, url) {
-    console.log('Your generated pre-signed URL is', url);
-});
-
 function HomeStackScreen(){
   return (
     <HomeStack.Navigator headerMode='none'>
