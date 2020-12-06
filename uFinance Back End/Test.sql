fc00ea2944bb90beeb79abe5da146f68265291ff
Rohan Kandi <kandirohan1@gmail.com>
2020-12-06 02:28:12 -0500

    Merging and push

--- uFinance Back End/Test.sql
+++ uFinance Back End/Test.sql
@@ -1,3 +1,4 @@
 -- INSERT INTO Users
--- VALUES ("david@yahoo.com", "abcdefg", "david", "David", "Silva", "2003-11-02 00:00:00");
-SELECT * FROM Users;
+-- VALUES ("david@yahoo.com", "abcdefg", "david", "David", "Silva", "2003-11-02 00:00:00");alter;
+-- SELECT * FROM Expense_Groups;
+SELECT * FROM Group_Members;
